cd0d9f46f7b3f74c7c725a239c6a7f59104f733b
Vardhan Adheli <88703752+vardhan1564@users.noreply.github.com>
2023-11-04 23:07:14 +0530

    Add files via upload

---
+++ TASK 2 = PortFolio/index2.js
@@ -0,0 +1,18 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+};
